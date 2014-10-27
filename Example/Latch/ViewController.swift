c8f80fa947aa68745dd0334b0fe7207d86705e81
Brian Vallelunga <vallelungabrian@gmail.com>
2014-10-26 19:56:58 -0700

    moved xcode project into example folder

---
+++ Example/Latch/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Latch
+//
+//  Created by Brian Vallelunga on 10/25/14.
+//  Copyright (c) 2014 Brian Vallelunga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, LatchDelegate {
+    
+    // MARK: Private Instance Variables
+    var latch: Latch!
+    
+    // MARK: UIViewController Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.latch = Latch()
+        self.latch.delegate = self
+        self.latch.parentController = self
+    }
+    
+    // MARK: IBAction
+    @IBAction func authorize(sender: UIButton) {
+        self.latch.enableTouch = true
+        self.latch.enablePasscode = true
+        self.latch.authorize()
+    }
+    
+    @IBAction func authorizePasscode(sender: UIButton) {
+        self.latch.enableTouch = false
+        self.latch.enablePasscode = true
+        self.latch.authorize()
+    }
+    
+    @IBAction func authorizeTouch(sender: UIButton) {
+        self.latch.enableTouch = true
+        self.latch.enablePasscode = false
+        self.latch.authorize()
+    }
+    
+    @IBAction func changePasscode(sender: UIButton) {
+        self.latch.updatePasscode()
+    }
+    
+    @IBAction func removePasscode(sender: UIButton) {
+        self.latch.removePasscode()
+    }
+    
+    // MARK: LatchDelegate Methods
+    func latchGranted() {
+        println("access granged")
+    }
+    
+    func latchSet() {
+        println("passcode set")
+    }
+    
+    func latchDenied(reason: LatchError) {
+        println(reason.hashValue)
+    }
+}
+
